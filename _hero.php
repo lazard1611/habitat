c73e344a56473beb343c653a8dc7dc3348bed09f
Lazar <dima.lazarev@cheitgroup.com>
2024-02-07 12:22:14 +0200

    init

--- _hero.php
+++
@@ -1,8 +0,0 @@
-<section class="section hero js-hero-section">
-	<div class="hero__in">
-		<h1 class="hero__title js-hero-title">We shape the future human habitat by designing innovation today.</h1>
-		<div class="hero__description_wrap">
-			<div class="hero__description js-hero-description">We start collaborating with you when you have just an idea and keep doing so when you are a 4B dollars unicorn.cc</div>
-		</div>
-	</div>
-</section>
--- _scripts.php
+++
@@ -1,9 +0,0 @@
-<script src="assets/libs/gsap.min.js"></script>
-<script src="assets/libs/ScrollTrigger.min.js"></script>
-<script src="assets/libs/ScrollSmoother.js"></script>
-<script src="assets/libs/SplitText.js"></script>
-<script src="assets/libs/TweenMax.min.js"></script>
-<!-- Scripts for frontend -->
-<script src="assets/js/scripts.js"></script>
-<!-- Scripts for backend -->
-<!--<script type='text/javascript' src="assets/js/app.min.js"></script>-->
